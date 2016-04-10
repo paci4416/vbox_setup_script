5194e4a4ba56069656a0ae168a2d2a8866fa10c0
paci4416 <yeudag@gmail.com>
2016-04-10 20:57:21 +0300

    Update vbox_setup.sh

--- vbox_setup.sh
+++ vbox_setup.sh
@@ -1,5 +1,16 @@
 cd ~
-sudo apt-get --assume-yes install git gcc make vim-gtk
+sudo apt-get --assume-yes install git gcc make vim-gtk 
+echo "git stuff..."
+echo "
+[alias]
+  co = checkout
+  ci = commit
+  st = status
+  br = branch
+  hist = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+  type = cat-file -t
+  dump = cat-file -p" >> ~/.gitconfig
+echo "vim stuff..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/paci4416/dotfiles
 cp dotfiles/vimrc ~/.vimrc
